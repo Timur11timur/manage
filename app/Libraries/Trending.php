e175320b163d6edfad79a83831324b2c9e209ead
Timur <t.fralik@aimprosoft.com>
2020-11-19 17:53:07 +0200

    Add usage generic cache driver

--- app/Libraries/Trending.php
+++ app/Libraries/Trending.php
@@ -3,29 +3,56 @@
 namespace App\Libraries;
 
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class Trending
 {
     public function get()
     {
-        return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 4));
+        //return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 4));
+        return Cache::get($this->cacheKey(), collect())
+            ->sortByDesc('score')
+            ->slice(0, 5)
+            ->values();
     }
 
-    public function push($thread)
+    public function push($thread, $increment = 1)
     {
-        Redis::zincrby($this->cacheKey(), 1, json_encode([
+//        Redis::zincrby($this->cacheKey(), 1, json_encode([
+//            'title' => $thread->title,
+//            'path' => $thread->path()
+//        ]));
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        $trending[$thread->id] = (object) [
+            'score' => $this->score($thread) + $increment,
             'title' => $thread->title,
-            'path' => $thread->path()
-        ]));
+            'path' => $thread->path(),
+        ];
+
+        Cache::forever($this->cacheKey(), $trending);
+    }
+
+    public function score($thread)
+    {
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        if (! isset($trending[$thread->id])) {
+            return 0;
+        }
+
+        return $trending[$thread->id]->score;
     }
 
     public function reset()
     {
-        Redis::del($this->cacheKey());
+        //Redis::del($this->cacheKey());
+        return Cache::forget($this->cacheKey());
     }
 
     private function cacheKey()
     {
-        return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
+        //return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
+        return 'trending_threads';
     }
 }
