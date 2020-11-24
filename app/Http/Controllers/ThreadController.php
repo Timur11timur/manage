d9697f0418d421b7346272a8afbc5d7d0d6a3a70
Timur <Timur11timur@users.noreply.github.com>
2020-11-24 17:04:48 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -156,6 +156,6 @@ class ThreadController extends Controller
             $threads->where('channel_id', $channel->id);
         }
 
-        return $threads->paginate(config('manage.pagination.perPage'));;
+        return $threads->paginate(config('manage.pagination.perPage'));
     }
 }
